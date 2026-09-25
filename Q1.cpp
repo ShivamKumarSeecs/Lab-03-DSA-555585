4a070c1b2de7d7e855d76a8915565d0b2953dce5
ShivamKumarSeecs <skumar.bscs25seecs@seecs.edu.pk>
2026-09-25 10:30:08 +0500

    Add Student struct and main function for input

---
+++ Q1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student //Student struct
+{
+	int rollNo;
+	string fullName; //fullname must be in string
+	float marks;
+};
+
+void main() {
+	Student s1; //declaration
+
+	//input
+	cout << "Enter Roll Number: ";
+	cin >> s1.rollNo;
+
+	cin.ignore(); //clear input buffer
+
+
+	cout << "Enter Full Name: ";
+	getline(cin, s1.fullName); //get whole line
+
+	cout << "Enter Marks: ";
+	cin >> s1.marks;
+
+	cout << endl; 
+
+	//labels
+	cout << "Roll Number: " << s1.rollNo << endl
+		<< "Full Name: " << s1.fullName << endl
+		<< "Marks: " << s1.marks << endl;
+
+}
